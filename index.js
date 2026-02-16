8c5bc1b2b8e3d23e083b46015eb7dd21b69ebb2e
Ankan83 <ankan.rastogi30@gmail.com>
2026-02-16 20:45:28 +0530

    Made changes for proper functionality

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-let saveEl = document.getElementById("save-el");
-let countEl = document.getElementById("count-el");
 let count = 0;
 
-function increment() {
-  count += 1;
-  countEl.textContent = count;
-}
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+let incrementBtn = document.getElementById("increment-btn");
+let saveBtn = document.getElementById("save-btn");
 
-function save() {
-  let countStr = count + " - ";
-  saveEl.textContent += countStr;
-  countEl.textContent = 0;
+incrementBtn.addEventListener("click", function () {
+  count++;
+  countEl.textContent = count;
+});
+
+saveBtn.addEventListener("click", function () {
+  saveEl.textContent += " " + count + " -";
   count = 0;
-}
+  countEl.textContent = 0;
+});
